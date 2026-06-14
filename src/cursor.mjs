44b57f9123d2d2ed1adcbea2e2e7ff80a3b5382c
Auke van Slooten <auke@muze.nl>
2026-06-14 16:34:33 +0200

    moved lower level code to note library

--- src/cursor.mjs
+++ src/cursor.mjs
@@ -13,89 +13,46 @@ export function sameLine(a, b)
 		a.end === b.end
 }
 
-export function handleArrowUp(note, previousNote=null, desiredX=null)
+export function moveUp(note, previousNote=null, desiredX=null)
 {
-	const selection = note.selection.get()
-
-	if (!selection) {
+	if (!previousNote) {
 		return false
 	}
+	if (!note.selection.moveUp()) {
+		if (desiredX === null) {
+			const caretPosition = note.selection.getCaretPosition()
+			desiredX = caretPosition.left
+		}
 
-	const offset = selection.focus
+		const previousLines = previousNote.selection.getVisualLines()
+		const lastLine = previousLines[previousLines.length - 1]
+		if (!lastLine) {
+			return false
+		}
 
-	const lines = note.selection.getVisualLines()
-
-	const lineIndex = findLineIndex(lines, offset)
-
-	if (lineIndex === -1) {
-		return false
+		previousNote.selection.setCaretPosition(lastLine, desiredX)
+		return true
 	}
-	
-	// Previous line in same note.
-	if (lineIndex > 0) {
-		return false // allow browser to do its normal thing
-	}
-
-	const caretPosition = offsetToPosition(note.editor, offset)
-
-	const previousLines = previousNote.selection.getVisualLines()
-
-	const lastLine = previousLines[previousLines.length - 1]
-	if (!lastLine) {
-		return false
-	}
-
-	if (desiredX === null) {
-		desiredX = caretPosition.left
-	}
-
-	previousNote.editor.focus()
-	const targetOffset = findClosestOffsetInLine(lastLine, desiredX)
-
-	previousNote.selection.set({focus:targetOffset})
-
-	return true
 }
 
-export function handleArrowDown(note, nextNote=null, desiredX=null)
+export function moveDown(note, nextNote=null, desiredX=null)
 {
 	if (!nextNote) {
 		return false
 	}
+	if (!note.selection.moveDown()) {
+		if (desiredX === null) {
+			const caretPosition = note.selection.getCaretPosition()
+			desiredX = caretPosition.left
+		}
 
-	const selection = note.selection.get()
-	if (!selection) {
-		return false
+		const nextLines = nextNote.selection.getVisualLines()
+		if (!nextLines.length) {
+			return false
+		}
+		const firstLine = nextLines[0]
+
+		nextNote.selection.setCaretPosition(firstLine, desiredX)
+		return true
 	}
-
-	const offset = selection.focus
-
-	const lines = note.selection.getVisualLines()
-	
-	const lineIndex = findLineIndex(lines, offset)
-	if (lineIndex === -1) {
-		return false
-	}
-
-	const caretPosition = offsetToPosition(note.editor,	offset)
-	if (lineIndex < lines.length - 1) {
-		return false
-	}
-
-	const nextLines = nextNote.selection.getVisualLines()
-	if (!nextLines.length) {
-		return false
-	}
-	const firstLine = nextLines[0]
-
-	if (desiredX === null) {
-		desiredX = caretPosition.left
-	}
-
-	nextNote.editor.focus()
-	const targetOffset = findClosestOffsetInLine(firstLine, desiredX)
-
-	nextNote.selection.set({focus: targetOffset})
-
-	return true
 }
