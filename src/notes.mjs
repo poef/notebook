90cff4d3a0227a6c7ae403fdd21b4e1218e2e42d
Auke van Slooten <auke@muze.nl>
2026-06-14 16:35:35 +0200

    all blocks now have a .block class

--- src/notes.mjs
+++ src/notes.mjs
@@ -1,9 +1,9 @@
 export default {
 	html: {
-		'note-h1': html`<h1 class="note" data-flow-field="note" data-simply-activate="note"></h1>`,
-		'note-p': html`<p class="note" data-flow-field="note" data-simply-activate="note"></p>`,
-		'code-js': html`<textarea class="code" data-helene-language="javascript" data-flow-field="code" data-simply-activate="code"></textarea>`,
-		'code-html': html`<textarea class="code" data-helene-language="html" data-flow-field="code" data-simply-activate="code"></textarea>`,
-		'code-css': html`<textarea class="code" data-helene-language="css" data-flow-field="code" data-simply-activate="code"></textarea>`
+		'note-h1': html`<h1 class="block note" data-flow-field="note" data-simply-activate="note"></h1>`,
+		'note-p': html`<p class="block note" data-flow-field="note" data-simply-activate="note"></p>`,
+		'code-js': html`<textarea class="block code" data-helene-language="javascript" data-flow-field="code" data-simply-activate="code"></textarea>`,
+		'code-html': html`<textarea class="block code" data-helene-language="html" data-flow-field="code" data-simply-activate="code"></textarea>`,
+		'code-css': html`<textarea class="block code" data-helene-language="css" data-flow-field="code" data-simply-activate="code"></textarea>`
 	}
 }
